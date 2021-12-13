a74398414477c6f678e4f0ffe6383f9701e893a3
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-13 14:11:27 +0100

    Update AC1_part1.cpp

--- Day1/AC1_part1.cpp
+++ Day1/AC1_part1.cpp
@@ -5,22 +5,20 @@
 #include <string>
 //---------------------------------ADVENT OF CODE 2021 - DAY 1 - PART I -----------------------------------
 int count_from_file(std::istream& str){ // Stream.
-    std::string line;
-
-    getline(str, line); // E.g. line = "163".
     if(!str.good()){
     	str.clear();
     	return -1;
-	}
+    }
 
+    std::string line;
     std::istringstream iss; // Input stream to operate on strings.
     int curr_num = 0, prev_num = std::numeric_limits<int>::max(), n_count = 1; // Numbers and counter.
 
-    for(;!str.eof();){
-        getline(str, line);
-        iss.clear();
-        iss.str(line);
-        iss >> curr_num;
+    for(;!str.eof();){      // Until the end of file...
+        getline(str, line); 
+        iss.clear();        // Clear and prepare the stream.
+        iss.str(line);      
+        iss >> curr_num;    // E.g. "163" --> 163.
         if(curr_num > prev_num) n_count++;
         prev_num = curr_num;
     }
