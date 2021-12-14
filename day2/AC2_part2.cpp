6dbf95806aa73e72e96d9e816e886f8ed8aa2c10
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-14 03:48:46 +0100

    Update AC2_part2.cpp

--- day2/AC2_part2.cpp
+++ day2/AC2_part2.cpp
@@ -12,26 +12,22 @@ long long int readFromFile(std::istream& str){
     std::string line, dir, mov; // Direction and movement.
     std::istringstream iss;
     int forwrd = 0, aim = 0;
-    long long depth = 0;
+    long long int depth = 0;
 
     for(;!str.eof();){
         getline(str, line);
         iss.clear();
         iss.str(line);
 	iss >> dir;
+	iss >> mov;
         if(dir == "forward"){
-            iss >> mov;
-            forwrd += (mov[0] - '0'); // Horizontal position.
+            forwrd += (mov[0] - '0');        // Horizontal position.
             depth += (aim * (mov[0] - '0')); // Depth.
         }
-        else if(dir == "up"){
-            iss >> mov;
+        else if(dir == "up")
             aim -= (mov[0] - '0');
-        }
-        else if(dir == "down"){
-            iss >> mov;
+        else if(dir == "down")
             aim += (mov[0] - '0');
-        }
 	else{
 	   std::cerr << "Invalid input format!\n";
 	   return -1;
