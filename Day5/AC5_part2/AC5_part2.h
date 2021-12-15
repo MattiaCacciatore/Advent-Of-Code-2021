d1b0554446e03ddb296f98789535f1da525187e0
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-15 02:05:41 +0100

    Add files via upload

---
+++ Day5/AC5_part2/AC5_part2.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>  // iss
+#include <fstream>  // ifs
+#include <string>
+#include <vector>
+#include <cstdlib>  // abs
+
+#define SIZE 1000
+//-------------------------------------------------FUNCTIONS-----------------------------------------------
+std::vector<std::vector<int>> read_vents_from_file(std::istream&);
+
+std::vector<std::vector<int>> create_map(size_t);
+
+void mark_all_vents(std::vector<std::vector<int>> &, std::vector<std::vector<int>> &v);
+
+long long int count_vents_in_map(std::vector<std::vector<int>> const &, size_t, size_t);
