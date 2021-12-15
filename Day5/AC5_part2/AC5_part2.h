383c4fc5702db431d03a24d90d423fac93f8fc40
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-15 02:06:09 +0100

    Update AC5_part2.h

--- Day5/AC5_part2/AC5_part2.h
+++ Day5/AC5_part2/AC5_part2.h
@@ -11,6 +11,6 @@ std::vector<std::vector<int>> read_vents_from_file(std::istream&);
 
 std::vector<std::vector<int>> create_map(size_t);
 
-void mark_all_vents(std::vector<std::vector<int>> &, std::vector<std::vector<int>> &v);
+void mark_all_vents(std::vector<std::vector<int>> &, std::vector<std::vector<int>> &);
 
 long long int count_vents_in_map(std::vector<std::vector<int>> const &, size_t, size_t);
