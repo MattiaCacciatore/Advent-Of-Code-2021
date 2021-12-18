445060bddb833c561c47b05e37e5ffedaf2d45a2
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-19 00:45:07 +0100

    Final version.

--- Day5/AC5_part2/AC5_part2_test.cpp
+++ Day5/AC5_part2/AC5_part2_test.cpp
@@ -1,12 +1,14 @@
-#include "AC5_part2.h"
+// Mattia Cacciatore - Computer Science student at the University of Genoa - Italy
+#include "AoC5_part2.h"
+//---------------------------------ADVENT OF CODE 2021 - DAY 5 - PART II ----------------------------------
 //---------------------------------------------------TEST--------------------------------------------------
 int main(){
-    std::string file = "input_day5.txt";
-    std::ifstream ifs(file.c_str());
-    std::vector<std::vector<int>> vents = read_vents_from_file(ifs); // Max_x = 988, max_y = 989.
-    std::vector<std::vector<int>> vents_map = create_map(SIZE);      // 322 horizontal/vertical vents, 168 diagonal vents.
+    std::vector<std::vector<precision>> vents = read_vents_from_file("input_day5.txt");
+    std::vector<std::vector<precision>> vents_map = create_map(SIZE);   // 322 horizontal/vertical vents, 168 diagonal vents.
     mark_all_vents(vents_map, vents);
-    long long int ans = count_vents_in_map(vents_map, SIZE, SIZE);
-    //std::cout << ans << "\n";
+    uint64_t ans = count_vents_in_map(vents_map, SIZE, SIZE);
+    std::cout << "***** ADVENT OF CODE 2021 - DAY 5 - PART II *****\n";
+    //std::cout << "\nThe number of dangerous vents is: " << ans << "\n"; // Tenth star **********
     return 0;
 }
+
