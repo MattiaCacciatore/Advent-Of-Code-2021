af0d54eae2206bdeb831fd4cdc683f342f1eead3
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-18 17:23:02 +0100

    Ehnanced version and rename it.

---
+++ day2/AoC2_part2.cpp
@@ -0,0 +1,44 @@
+// Mattia Cacciatore - Computer Science student at Genoa University - Italy
+#include <iostream>
+#include <sstream>  // iss
+#include <fstream>  // ifs
+#include <string>
+//---------------------------------ADVENT OF CODE 2021 - DAY 2 - PART II -----------------------------------
+uint64_t read_path_from_file(std::string input){
+    std::ifstream ifs(input);
+    uint64_t forwrd = 0, aim = 0, depth = 0;
+    if(ifs.good()){
+        std::string line, dir, mov;              // Direction and movement.
+        std::istringstream iss;
+
+        for(;!ifs.eof();){
+            getline(ifs, line);
+            iss.clear();
+            iss.str(line);
+            iss >> dir;
+            if(dir == "forward"){
+                iss >> mov;
+                forwrd += (mov[0] - '0');        // Horizontal position.
+                depth += (aim * (mov[0] - '0')); // Depth.
+            }
+            else if(dir == "up"){
+                iss >> mov;
+                aim -= (mov[0] - '0');
+            }
+            if(dir == "down"){
+                iss >> mov;
+                aim += (mov[0] - '0');
+            }
+        }
+    }
+    ifs.clear();
+    depth *= forwrd;
+    return depth ;
+}
+//---------------------------------------------------TEST--------------------------------------------------
+int main(){
+    uint64_t ans = read_path_from_file("input_day2.txt");
+    std::cout << "***** ADVENT OF CODE 2021 - DAY 2 - PART II *****\n";
+    //std::cout << "\nCurrent depth level of the submarine: " << ans << "\n"; // Fourth star ****
+    return 0;
+}
