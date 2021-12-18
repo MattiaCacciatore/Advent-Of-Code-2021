f06eda7d8960c3f283793b742634d95c4b16a2e4
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-19 00:31:41 +0100

    Final version.

---
+++ Day5/AC5_part1/AoC5_part1.h
@@ -0,0 +1,18 @@
+// Mattia Cacciatore - Computer Science student at the University of Genoa - Italy
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define SIZE 1000           // Be aware of precision. Map size.
+
+typedef uint16_t precision; 
+//-------------------------------------------------FUNCTIONS-----------------------------------------------
+std::vector<std::vector<precision>> read_vents_from_file(std::string);
+
+std::vector<std::vector<precision>> create_map(size_t);
+
+void mark_all_vents(std::vector<std::vector<precision>> &, std::vector<std::vector<precision>> &);
+
+uint64_t count_vents_in_map(std::vector<std::vector<precision>> const &, size_t, size_t);
