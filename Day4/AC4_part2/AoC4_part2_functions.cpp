b277430682d171fae449e4ef590c0437e5477b01
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-19 00:06:01 +0100

    Final version.

--- Day4/AC4_part2/AoC4_part2_functions.cpp
+++ Day4/AC4_part2/AoC4_part2_functions.cpp
@@ -1,14 +1,15 @@
-#include "AC4_part2.h"
+// Mattia Cacciatore - Computer Science student at the University of Genoa - Italy
+#include "AoC4_part2.h"
 //---------------------------------ADVENT OF CODE 2021 - DAY 4 - PART II ----------------------------------
 //-------------------------------------------HELPER FUNCTIONS----------------------------------------------
-std::vector<std::vector<short int>> create_card(){
-    std::vector<std::vector<short int>> matrix;
+std::vector<std::vector<precision>> create_card(){
+    std::vector<std::vector<precision>> matrix;
     matrix.resize(BINGO_SIZE);
     for(size_t i = 0; i < BINGO_SIZE; ++i){matrix[i].resize(BINGO_SIZE);}
     return matrix;
 }
 
-bool check_bingo(std::vector<std::vector<short int>> const &card){
+bool check_bingo(std::vector<std::vector<precision>> const &card){
     for(size_t r = 0, c = 0; r < BINGO_SIZE; ++r){ // Row checking.
         for(c = 0; c < BINGO_SIZE; ++c){
             if(card[r][c] != -1) break;
@@ -25,77 +26,75 @@ bool check_bingo(std::vector<std::vector<short int>> const &card){
     return false;
 }
 
-bool mark_number(std::vector<std::vector<short int>> &card, short int n){
+bool mark_number(std::vector<std::vector<precision>> &card, precision n){
     for(size_t r = 0, c = 0; r < BINGO_SIZE; ++r){
         for(c = 0; c < BINGO_SIZE; ++c){
-            if(card[r][c] == n) card[r][c] = -1; // Marks the number then it checks if
-            if(check_bingo(card)) return true;   // we got a winning card.
+            if(card[r][c] == n) card[r][c] = -1; // Marks the number.
+            if(check_bingo(card)) return true;
         }
     }
     return false;
 }
 
-int sum_all_numbers(std::vector<std::vector<short int>> const &card){
-    int sum = 0;
+uint32_t sum_all_numbers(std::vector<std::vector<precision>> const &card){
+    uint32_t sum = 0;
     for(size_t r = 0, c = 0; r < BINGO_SIZE; ++r){
         for(c = 0; c < BINGO_SIZE; ++c){
-            if(card[r][c] != -1) sum += card[r][c];
+            if(card[r][c] > 0) sum += card[r][c];
         }
     }
     return sum;
 }
 //-------------------------------------------------FUNCTIONS-----------------------------------------------
-std::vector<std::vector<std::vector<short int>>> read_cards_from_file(std::istream& str){
-    std::vector<std::vector<std::vector<short int>>> cards;
-    if(str.good()){
+std::vector<std::vector<std::vector<precision>>> read_cards_from_file(std::string input){
+    std::vector<std::vector<std::vector<precision>>> cards;
+    std::ifstream ifs(input);
+    if(ifs.good()){
         std::string line;
         std::istringstream iss;                                     // Input stream to operate on string.
-        std::vector<std::vector<short int>> card = create_card();
+        std::vector<std::vector<precision>> card = create_card();
 
-        for(;!str.eof();){
-            for(int c = 0, r = 0; r < BINGO_SIZE; ++r){             // Column and row.
-                getline(str, line);
+        for(;!ifs.eof();){
+            for(uint32_t c = 0, r = 0; r < BINGO_SIZE; ++r){        // Column and row.
+                getline(ifs, line);
                 iss.clear();
                 iss.str(line);
                 for(c = 0; c < BINGO_SIZE; ++c){iss >> card[r][c];} // Inserting a single row.
             }
-            str.clear();
-            getline(str, line);                                     // Empty space between cards.
-            if(line != ""){
-                std::cerr << "Invalid input format!\n";
-                cards.resize(0);
-                return cards;
-            }
-            cards.push_back(card);                                  // Inserting the new card.
+            getline(ifs, line);                                     // Empty space between cards.
+            cards.push_back(card);
         }
     }
-    str.clear();
+    ifs.clear();
     return cards;
 }
 
-std::vector<short int> read_numbers_from_file(std::istream& str){
-    std::vector<short int> w_n; // Wild numbers.
-    if(str.good()){
-        short int n;
+std::vector<precision> read_numbers_from_file(std::string input){
+    std::vector<precision> w_n; // Wild numbers.
+    std::ifstream ifs(input);
+    if(ifs.good()){
+        precision n;
+        char garbage;           // See input.
         std::string line;
-        std::istringstream iss; // Input stream to operate on string.
+        std::istringstream iss;
 
-        getline(str, line);     // In a single line (see input).
+        getline(ifs, line);     // In a single line.
         iss.clear();
         iss.str(line);
-        iss >> n;               // Extracting the i-th number.
+        iss >> n;               // Extracting the i-th number...
         for(;iss;){
-            w_n.push_back(n);   // and pushing it into vector.
+	        iss >> garbage;     // ,
+            w_n.push_back(n);   // ...and pushing it in the vector.
             iss >> n;
         }
     }
-    str.clear();
+    ifs.clear();
     return w_n;
 }
 
-long long int lose_bingo(std::vector<std::vector<std::vector<short int>>> &cards, std::vector<short int> const &numbers){
-    std::vector<std::vector<std::vector<short int>>> winning_cards;
-    std::vector<short int> winning_numbers;
+uint64_t lose_bingo(std::vector<std::vector<std::vector<precision>>> &cards, std::vector<precision> const &numbers){
+    std::vector<std::vector<std::vector<precision>>> winning_cards;
+    std::vector<precision> winning_numbers;
     for(size_t i = 0, prev = 0, rounds = numbers.size(); i < rounds; ++i){
         for(size_t j = 0, n_c = cards.size(); j < n_c; ++j){ // Number of cards.
             if(mark_number(cards[j], numbers[i])){           // Marks all cards with the i-th wild number.
@@ -107,8 +106,9 @@ long long int lose_bingo(std::vector<std::vector<std::vector<short int>>> &cards
             }
         }
     }
-    int sum = sum_all_numbers(winning_cards[winning_cards.size() - 1]);  // Last winning card is what we're looking for.
-    short int last_number = winning_numbers[winning_numbers.size() - 1]; // Same here.
-    long long int res = sum * last_number;
+    uint32_t sum = sum_all_numbers(winning_cards[winning_cards.size() - 1]);  // Last winning card is what we're looking for.
+    precision last_number = winning_numbers[winning_numbers.size() - 1];      // Same here.
+    uint64_t res = sum * last_number;
     return res;
 }
+
