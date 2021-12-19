731f8fe778ff10b055c8ea3510f5cf28ef99e80a
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-19 03:05:25 +0100

    Final version.

--- Day8/AC8_part2/AoC8_part2.h
+++ Day8/AC8_part2/AoC8_part2.h
@@ -1,14 +1,15 @@
+// Mattia Cacciatore - Computer Science student at the University of Genoa - Italy
 #include <iostream>
 #include <sstream>   // iss
 #include <fstream>   // ifs
 #include <string>
 #include <vector>
 
-#define INP_SIZE 14  // Number of strings in the input line.
+#define DIG_SIZE 14  // Number of strings in the input line.
 //----------------------------------------------FUNCTIONS--------------------------------------------------
 // Just for fun.
 void print_digit(std::vector<char> const);
 
-std::vector<std::vector<std::string>> read_digit_from_file(std::istream&);
+std::vector<std::vector<std::string>> read_digit_from_file(std::string);
 
-long long int sum_all_digits(std::vector<std::vector<std::string>> const&);
+uint64_t sum_all_digits(std::vector<std::vector<std::string>> const&);
