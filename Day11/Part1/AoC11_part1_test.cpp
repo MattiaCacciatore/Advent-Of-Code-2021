2ec4b9ba5b70e2553113b587626ec632d02db837
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-23 18:10:36 +0100

    Best attempt.

--- Day11/Part1/AoC11_part1_test.cpp
+++ Day11/Part1/AoC11_part1_test.cpp
@@ -6,6 +6,6 @@ int main(){
     read_cavern_from_file("input_day11.txt");
     uint64_t ans = count_flashes(steps);
     std::cout << "***** ADVENT OF CODE 2021 - DAY 11 - PART I *****\n";
-    //std::cout << "\nThe total amount of flashes is: UNKNOWN\n";
+    //std::cout << "\nThe total amount of flashes is: UNKNOWN\n"; // Read the README file.
     return 0;
 }
