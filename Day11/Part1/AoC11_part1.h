ae3c634f7db1763d86a5340e862278b75511e0c6
Mattia Cacciatore <73939270+MattiaCacciatore@users.noreply.github.com>
2021-12-23 17:40:04 +0100

    Best attempt.

---
+++ Day11/Part1/AoC11_part1.h
@@ -0,0 +1,18 @@
+// Mattia Cacciatore - Computer Science student at the University of Genoa - Italy
+#include <iostream>
+#include <sstream>   // iss
+#include <fstream>   // ifs
+#include <string>
+
+#define CAVERN_SIZE 10
+
+struct dumbo_octopus{
+    int energy;
+    bool flashed;
+};
+
+dumbo_octopus cavern[CAVERN_SIZE][CAVERN_SIZE]; // Fixed input.
+//------------------------------------------------FUNCTIONS------------------------------------------------
+void read_cavern_from_file(std::string);
+
+uint64_t count_flashes(uint64_t);
